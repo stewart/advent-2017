9b72df0715727b366cb747290cd7e730e93af0a6
Andrew Stewart <andrew@stwrt.ca>
2017-12-02 18:35:13 -0800

    Do a bit of prep work for Day 3
    Just set up using `include_str!` for input, and a placeholder test.

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input");
+    println!("{}", input);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_something() {
+        assert_eq!(1, 1);
+    }
 }
