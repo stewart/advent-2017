018647d5642136774d577cb59539d5a5163f6d31
Andrew Stewart <andrew@stwrt.ca>
2017-12-04 21:05:19 -0800

    Basic scaffolding

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("input");
+    println!("{}", input);
+}
+
+fn escape(jmplist: Vec<i32>) -> usize {
+    5
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_escape() {
+        assert_eq!(escape(vec![0, 3, 0, 1, -3]), 5);
+    }
 }
