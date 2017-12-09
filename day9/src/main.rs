8b5de6e90e318cf7910760c6aacd7a74c05296fe
Andrew Stewart <andrew@stwrt.ca>
2017-12-09 10:15:40 -0800

    Add basics for Day 9

---
+++ day9/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let input = include_str!("input");
+    println!("{}", input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_groups() {
+        assert_eq!(1, 1);
+    }
+}
