1e4615ca28b45bf1160c16ad7bb79d228f84f901
Andrew Stewart <andrew@stwrt.ca>
2017-12-09 10:35:00 -0800

    Part 1 of Day 9

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,6 +1,32 @@
 fn main() {
     let input = include_str!("input");
-    println!("{}", input);
+    println!("1 -> {}", score(input));
+}
+
+fn score(stream: &str) -> usize {
+    let mut score = 0;
+    let mut level = 0;
+
+    let mut garbage = false;
+
+    let mut chars = stream.chars();
+
+    while let Some(ch) = chars.next() {
+        match ch {
+            '!' => { chars.next(); },
+
+            '>' => garbage = false,
+            _ if garbage => {},
+            '<' => garbage = true,
+
+            '{' => { level += 1 },
+            '}' => { score += level; level -= 1; }
+
+            _ => {},
+        }
+    }
+
+    score
 }
 
 #[cfg(test)]
@@ -8,7 +34,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_groups() {
-        assert_eq!(1, 1);
+    fn test_score() {
+        assert_eq!(score("{}"), 1);
+        assert_eq!(score("{{{}}}"), 6);
+        assert_eq!(score("{{},{}}"), 5);
+        assert_eq!(score("{{{},{},{{}}}}"), 16);
+        assert_eq!(score("{<a>,<a>,<a>,<a>}"), 1);
+        assert_eq!(score("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
+        assert_eq!(score("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
+        assert_eq!(score("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
     }
 }
