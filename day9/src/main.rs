c9dc5b5a209e7e2b70b658a78d0fcfb2ff2b7ea8
Andrew Stewart <andrew@stwrt.ca>
2017-12-09 10:38:04 -0800

    Day 9, Part Two
    Glad I'm using a language with pattern matching!

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
     let input = include_str!("input");
-    println!("1 -> {}", score(input));
+    let (score, garbage) = score(input);
+
+    println!("1 -> {}", score);
+    println!("2 -> {}", garbage);
 }
 
-fn score(stream: &str) -> usize {
+fn score(stream: &str) -> (usize, usize) {
     let mut score = 0;
     let mut level = 0;
 
     let mut garbage = false;
+    let mut garbage_score = 0;
 
     let mut chars = stream.chars();
 
@@ -16,7 +20,7 @@ fn score(stream: &str) -> usize {
             '!' => { chars.next(); },
 
             '>' => garbage = false,
-            _ if garbage => {},
+            _ if garbage => { garbage_score += 1 },
             '<' => garbage = true,
 
             '{' => { level += 1 },
@@ -26,7 +30,7 @@ fn score(stream: &str) -> usize {
         }
     }
 
-    score
+    (score, garbage_score)
 }
 
 #[cfg(test)]
@@ -35,13 +39,13 @@ mod tests {
 
     #[test]
     fn test_score() {
-        assert_eq!(score("{}"), 1);
-        assert_eq!(score("{{{}}}"), 6);
-        assert_eq!(score("{{},{}}"), 5);
-        assert_eq!(score("{{{},{},{{}}}}"), 16);
-        assert_eq!(score("{<a>,<a>,<a>,<a>}"), 1);
-        assert_eq!(score("{{<ab>},{<ab>},{<ab>},{<ab>}}"), 9);
-        assert_eq!(score("{{<!!>},{<!!>},{<!!>},{<!!>}}"), 9);
-        assert_eq!(score("{{<a!>},{<a!>},{<a!>},{<ab>}}"), 3);
+        assert_eq!(score("{}"), (1, 0));
+        assert_eq!(score("{{{}}}"), (6, 0));
+        assert_eq!(score("{{},{}}"), (5, 0));
+        assert_eq!(score("{{{},{},{{}}}}"), (16, 0));
+        assert_eq!(score("{<a>,<a>,<a>,<a>}"), (1, 4));
+        assert_eq!(score("{{<ab>},{<ab>},{<ab>},{<ab>}}"), (9, 8));
+        assert_eq!(score("{{<!!>},{<!!>},{<!!>},{<!!>}}"), (9, 0));
+        assert_eq!(score("{{<a!>},{<a!>},{<a!>},{<ab>}}"), (3, 17));
     }
 }
