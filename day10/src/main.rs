471e569fc0406ace039d910bb8428537fcab9447
Andrew Stewart <andrew@stwrt.ca>
2017-12-11 09:42:27 -0800

    Day 10, Part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -2,18 +2,34 @@ const LEN: usize = 256;
 
 fn main() {
     let input = include_str!("input").trim();
+    println!("1 -> {:?}", part1(&input));
+    println!("2 -> {:?}", part2(&input));
+}
 
+fn part1(input: &str) -> usize {
     let input = input.
         split(',').
         map(|i| i.parse::<usize>().unwrap()).
         collect::<Vec<usize>>();
 
-    println!("1 -> {:?}", part1(&input));
+    let list = knots(&input, 1);
+    list[0] * list[1]
 }
 
-fn part1(input: &[usize]) -> usize {
-    let list = knots(input, 1);
-    list[0] * list[1]
+fn part2(input: &str) -> String {
+    let mut input = input.
+        bytes().
+        map(|b| b as usize).
+        collect::<Vec<_>>();
+
+    input.extend(vec![17, 31, 73, 47, 23]);
+
+    let list = knots(&input, 64).
+        chunks(16).
+        map(|chunk| format!("{:02x}", chunk.iter().fold(0, |a, b| a ^ b))).
+        collect::<Vec<_>>();
+
+    list.join("")
 }
 
 fn knots(input: &[usize], rounds: usize) -> Vec<usize> {
