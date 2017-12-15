e08d7d32f1ebf1bce63a29131791ca851847b198
Andrew Stewart <andrew@stwrt.ca>
2017-12-15 10:33:19 -0800

    Refactor Day 15

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,59 +1,71 @@
 struct Generator {
     value: usize,
+    factor: usize
+}
+
+impl Generator {
+    fn new(value: usize, factor: usize) -> Generator {
+        Generator { value: value, factor: factor }
+    }
+
+    fn next(&mut self) -> u16 {
+        self.value = (self.value * self.factor) % 2147483647;
+        self.value as u16
+    }
+}
+
+struct PickyGenerator {
+    value: usize,
     factor: usize,
     multiplier: usize
 }
 
-impl Generator {
-    fn new(value: usize, factor: usize, multiplier: usize) -> Generator {
-        Generator { value: value, factor: factor, multiplier: multiplier }
+impl PickyGenerator {
+    fn new(value: usize, factor: usize, multiplier: usize) -> PickyGenerator {
+        PickyGenerator { value: value, factor: factor, multiplier: multiplier }
     }
 
-    fn next(&mut self) -> Option<u16> {
-        self.value = (self.value * self.factor) % 2147483647;
-
-        if self.value % self.multiplier == 0 {
-            Some(self.value as u16)
-        } else {
-            None
+    fn next(&mut self) -> u16 {
+        loop {
+            self.value = (self.value * self.factor) % 2147483647;
+            if self.value % self.multiplier == 0 {
+                return self.value as u16;
+            }
         }
     }
 }
 
 fn main() {
-    let mut a = Generator::new(116, 16807, 4);
-    let mut b = Generator::new(299, 48271, 8);
+    println!("1 -> {}", part1());
+    println!("2 -> {}", part2());
+}
+
+fn part1() -> usize {
+    let mut a = Generator::new(116, 16807);
+    let mut b = Generator::new(299, 48271);
 
     let mut matches = 0;
 
-    for _ in 0..5_000_000 {
-        let a_value: u16;
-        let b_value: u16;
-
-        loop {
-            match a.next() {
-                Some(a) => {
-                    a_value = a;
-                    break;
-                },
-                None => {}
-            }
-        }
-
-        loop {
-            match b.next() {
-                Some(b) => {
-                    b_value = b;
-                    break;
-                },
-                None => {}
-            }
-        }
-
-        if a_value == b_value {
+    for _ in 0..40_000_000 {
+        if a.next() == b.next() {
             matches += 1;
         }
     }
 
-    println!("2 -> {}", matches)
+    matches
+}
+
+fn part2() -> usize {
+    let mut a = PickyGenerator::new(116, 16807, 4);
+    let mut b = PickyGenerator::new(299, 48271, 8);
+
+    let mut matches = 0;
+
+    for _ in 0..5_000_000 {
+        if a.next() == b.next() {
+            matches += 1;
+        }
+    }
+
+    matches
 }
