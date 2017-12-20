92b56b1026a7f03806d8c071e179c72e812eb5af
Andrew Stewart <andrew@stwrt.ca>
2017-12-19 18:17:06 -0800

    Parse input

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -1,5 +1,13 @@
 mod op;
 
+use op::{Op, Value};
+
 fn main() {
-    println!("Hello, world!");
+    let ops: Vec<Op> = include_str!("input").
+        trim().
+        lines().
+        map(|line| line.parse().unwrap()).
+        collect();
+
+    println!("1 -> {:?}", ops);
 }
