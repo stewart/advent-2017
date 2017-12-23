89c499172ac870baf1b109a318a37f67deaae00d
Andrew Stewart <andrew@stwrt.ca>
2017-12-23 15:58:44 -0800

    Day 18, Part 1

--- day18/src/main.rs
+++ day18/src/main.rs
@@ -17,14 +17,92 @@ impl Cpu {
         let mut registers = HashMap::new();
 
         for ch in "abcdefghijklmnop".chars() {
-            registers.insert(ch, initial_value);
+            registers.insert(ch, 0);
         }
+
+        registers.insert('p', initial_value);
 
         Cpu {
             registers: registers,
             operations: operations,
             idx: 0,
             played: 0,
+        }
+    }
+
+    fn get(&self, value: Value) -> isize {
+        use Value::*;
+
+        match value {
+            Reg(v) => {
+                self.registers.get(&v).cloned().expect("Unexpected register")
+            }
+            Const(v) => v
+        }
+    }
+
+    fn set(&mut self, register: char, value: isize) {
+        self.registers.insert(register, value);
+    }
+
+    fn apply(&mut self, op: Op) {
+        use Op::*;
+        use Value::*;
+
+        match op {
+            Add(Reg(a), b) => {
+                let value = self.get(Reg(a)) + self.get(b);
+                self.set(a, value);
+                self.idx += 1;
+            }
+
+            Jgz(a, b) => {
+                let a = self.get(a);
+                let b = self.get(b);
+
+                let jump: isize = if a > 0 { b } else { 1 };
+                self.idx = (self.idx as isize + jump) as usize;
+            }
+
+            Mod(Reg(a), b) => {
+                let value = self.get(Reg(a)) % self.get(b);
+                self.set(a, value);
+                self.idx += 1;
+            }
+
+            Mul(Reg(a), b) => {
+                let value = self.get(Reg(a)) * b;
+                self.set(a, value);
+                self.idx += 1;
+            }
+
+            Rcv(a) => {
+                if self.get(a) != 0 {
+                    println!("Recovered: {}", self.played);
+                }
+                self.idx += 1;
+            }
+
+            Set(Reg(a), b) => {
+                let value = self.get(b);
+                self.set(a, value);
+                self.idx += 1;
+            }
+
+            Snd(a) => {
+                let value = self.get(a);
+                self.played = value;
+                self.idx += 1;
+            }
+
+            _ => panic!("Unexpected op: {:?}", op)
+        }
+    }
+
+    fn run(&mut self) {
+        while self.idx >= 0 && self.idx < self.operations.len() {
+            let op = self.operations[self.idx].clone();
+            self.apply(op);
         }
     }
 }
@@ -36,7 +114,7 @@ fn main() {
         map(|line| line.parse().unwrap()).
         collect();
 
-    let cpu = Cpu::new(0, ops.clone());
+    let mut cpu = Cpu::new(0, ops.clone());
 
-    println!("1 -> {:?}", cpu);
+    cpu.run();
 }
